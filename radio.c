531eafe9a596b264261f9809eb8807ebdd46981b
KD Diallo <superkd02@gmail.com>
2026-02-24 14:13:29 +0100

    Fix channels being skipped while scanning

--- radio.c
+++ radio.c
@@ -105,11 +105,12 @@ bool RADIO_CheckValidChannel(uint16_t channel, bool checkScanList, uint8_t scanL
     //return true;
 
     // I don't understand what this code is for...
-    
+    // What happens when scanList equals 0 or 4? Most likely reading a 0 value, which means channel number 0 will be skipped.
     const uint8_t PriorityCh1 = gEeprom.SCANLIST_PRIORITY_CH1[scanList - 1];
     const uint8_t PriorityCh2 = gEeprom.SCANLIST_PRIORITY_CH2[scanList - 1];
 
-    return PriorityCh1 != channel && PriorityCh2 != channel;
+    return (scanList == 0 || scanList == 4) || // the proposed fix
+    (PriorityCh1 != channel && PriorityCh2 != channel);
 }
 
 uint8_t RADIO_FindNextChannel(uint8_t Channel, int8_t Direction, bool bCheckScanList, uint8_t VFO)
